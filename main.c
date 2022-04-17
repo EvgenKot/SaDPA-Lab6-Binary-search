7277c8233a514c475b19d847a894e8d625857c1e
Evgeny Kot <ukal.evgen@gmail.com>
2022-04-17 19:11:13 +0500

    v0.10.0
    Переформатировано
    Добавлены комментарии

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /*
 Ращупкин Евгений КЭ-203
 2 (28) вариант
@@ -16,28 +15,32 @@ Lab6
 Реализовать программу с помощью следующих алгоритмов: бинарный поиск (для варианта 2).
 */
 
-
-void PrintArray(int *arr, int n){
-    for (int i = 0; i<n; i++){
+//Вывод массива
+void PrintArray(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-
-
-
-void RandArr(int *arr, int size) {
+//Заполнение массива случайными элементами
+void RandArr(int *arr, int size)
+{
     int i;
-    for (i = 0; i < size; i++){
+    for (i = 0; i < size; i++)
+    {
         arr[i] = rand();
     }
-    
 }
 
-int LeftBinarySearch(int key, int *arr, int size) {
+//Поиск совпадения по левому элементом
+int LeftBinarySearch(int key, int *arr, int size)
+{
     int left = 0, right = size;
-    while (abs(right - left) > 1) {
+    while (abs(right - left) > 1)
+    {
         int middle = (left + right) / 2;
         if (key <= arr[middle])
             right = middle;
@@ -50,9 +53,12 @@ int LeftBinarySearch(int key, int *arr, int size) {
         return -1;
 }
 
-int RightBinarySearch(int key, int *arr, int size) {
+//Поиск совпадения по правому элементом
+int RightBinarySearch(int key, int *arr, int size)
+{
     int left = 0, right = size;
-    while (abs(right - left) > 1) {
+    while (abs(right - left) > 1)
+    {
         int middle = (left + right) / 2;
         if (key >= arr[middle])
             left = middle;
@@ -65,9 +71,9 @@ int RightBinarySearch(int key, int *arr, int size) {
         return -1;
 }
 
-
-
-void qsortRecursive(int *arr, int size) {
+//Быстрая сортировка
+void qsortRecursive(int *arr, int size)
+{
     //Указатели в начало и в конец массива
     int i = 0;
     int j = size - 1;
@@ -76,19 +82,23 @@ void qsortRecursive(int *arr, int size) {
     int mid = arr[size / 2];
 
     //Делим массив
-    do {
+    do
+    {
         //Пробегаем элементы, ищем те, которые нужно перекинуть в другую часть
         //В левой части массива пропускаем(оставляем на месте) элементы, которые меньше центрального
-        while(arr[i] < mid) {
+        while (arr[i] < mid)
+        {
             i++;
         }
         //В правой части пропускаем элементы, которые больше центрального
-        while(arr[j] > mid) {
+        while (arr[j] > mid)
+        {
             j--;
         }
 
         //Меняем элементы местами
-        if (i <= j) {
+        if (i <= j)
+        {
             int tmp = arr[i];
             arr[i] = arr[j];
             arr[j] = tmp;
@@ -98,20 +108,21 @@ void qsortRecursive(int *arr, int size) {
         }
     } while (i <= j);
 
-
-    //Рекурсивные вызовы, если осталось, что сортировать
-    if(j > 0) {
-        //"Левый кусок"
+    //Рекурсивные вызовы
+    if (j > 0)
+    {
+        //Слева
         qsortRecursive(arr, j + 1);
     }
-    if (i < size) {
-        //"Првый кусок"
+    if (i < size)
+    {
+        //Справа
         qsortRecursive(&arr[i], size - i);
     }
 }
 
-
-int main() {
+int main()
+{
     int n;
     int key;
 
@@ -119,33 +130,33 @@ int main() {
     scanf("%d", &n);
 
     int *arr = malloc(n * sizeof(int));
-    RandArr (arr, n);
-    PrintArray (arr, n);
+    RandArr(arr, n);
+    PrintArray(arr, n);
 
     printf("Sorted:\n");
     qsortRecursive(arr, n);
-    PrintArray (arr, n);
+    PrintArray(arr, n);
 
     printf("Key: ");
     scanf("%d", &key);
     int i_start = LeftBinarySearch(key, arr, n);
     int i_finish = RightBinarySearch(key, arr, n);
-    if (i_start == -1){
+    if (i_start == -1)
+    {
         printf("No matching");
     }
-    else if (i_start == i_finish){
-        printf("arr[%d] = %d \n", i_start, arr[i_start]);    
+    else if (i_start == i_finish)
+    {
+        printf("arr[%d] = %d \n", i_start, arr[i_start]);
     }
-    else {
+    else
+    {
         printf("arr[%d] = %d \n", i_start, arr[i_start]);
         printf("...\n");
         printf("arr[%d] = %d \n", i_finish, arr[i_finish]);
         printf("%d matching\n", i_finish - i_start + 1);
     }
-    
 
-    
-    
     free(arr);
     return 0;
 }
